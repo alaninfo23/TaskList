cb8acbdf7d0c7d36a0cb7bb14d089d7f9241f46e
Alan Santos <js-alan@hotmail.com>
2023-01-30 16:28:16 -0300

    adicionando comentários

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 const inputElement = document.querySelector('.new-task-input');
 const addTaskButton = document.querySelector('.new-task-button');
 
+/* Seleciona a classe task-container p/ adicionar parágrafo e icone de deletar */
 const tasksContainer = document.querySelector('.task-container');
 
 /* Função que valida se o valor digitado no input for maior que 0 ele retorna false*/
@@ -17,40 +18,49 @@ const handleAddTask = () => {
     if (!inputIsvalid) {
         return inputElement.classList.add('error');
     }
-
     /* Essa função cria uma elemento DIV no HTML e atribui a variavel taskItemContainer */
     const taskItemContainer = document.createElement('div');
-    /* Essa função adiciona uma classe para a DIV que foi criada */
+
+    /* Essa função adiciona uma classe para a DIV que foi criada acima*/
     taskItemContainer.classList.add('task-item');
 
     /* Essa função cria um parágrafo onde vai aparecer o conteúdo do input  */
     const taskContent = document.createElement('p');
-    /* Essa função insere o valor do input no parágrafo que foic riado com a função anterior */
+
+    /* Essa função insere o valor do input no parágrafo que foi criado com a função anterior */
     taskContent.innerText = inputElement.value;
 
+    /* Essa função espera um clique no taskContent(tarefa) para executar a função Handle Click */
     taskContent.addEventListener('click', () => handleClick(taskContent));
 
+    /* Essa variavel recebe o elemento do icone de deletar */
     const deleteItem = document.createElement('i');
+    /* E ai a variavel recebe duas classes referente ao icone e atribui a variavel que foi criada acima. */
+    /* As informações desse icone estão lá no arquivo CSS */
     deleteItem.classList.add('far');
     deleteItem.classList.add('fa-trash-alt');
 
-    deleteItem.addEventListener('click', () =>
-    handleDeleteClick(taskItemContainer, taskContent)
-    );
+    /* Essa função espera um clique no deleteItem(botão deletar) para executar a função handleDeleteClick */
+    deleteItem.addEventListener('click', () => handleDeleteClick(taskItemContainer, taskContent));
 
+    /* Essas 2 funções adicionam a classe task-item e o paragrafo P que foram criados
+    acima dentro da DIV taskItemContainer. */
     taskItemContainer.appendChild(taskContent);
     taskItemContainer.appendChild(deleteItem);
-
+    /* Essa função adiciona a div taskItemContainer no tasksContainer   */
     tasksContainer.appendChild(taskItemContainer);
-
+    /* Essa função apenas insere um valor vazio no input para limpar o campo após clicar em adicionar */
     inputElement.value = '';
     
     updateLocalStorage();
 };
 
+/* Essa função percorre os elementos do taskitem até achar o correspondente
+ao taskContent, em seguida ele mudará o nome da classe*/
 const handleClick = (taskContent) => {
+    /* Essa função pega todos os filhos do taskitem */
     const tasks = tasksContainer.childNodes;
-  
+    /* Verificar se o item atual do loop é o mesmo que está sendo clicado, se for ele muda nome da classe para completed. */
     for (const task of tasks) {
       const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
   
@@ -58,19 +68,23 @@ const handleClick = (taskContent) => {
         task.firstChild.classList.toggle("completed");
       }
     }
-  
+  /* Executando função que armazena as alterações feitas no LocalStorage */
     updateLocalStorage();
   };
+
+/* Essa função faz a mesma coisa da função acima só que ao invés de mudar nome da classe ele remove o Taskitem */
 const handleDeleteClick = (taskItemContainer, taskContent) => {
+  /* Essa função pega todos os filhos do taskitem */
     const tasks = tasksContainer.childNodes;
     for (const task of tasks) {
+      /* Verificar se o item atual do loop é o mesmo que está sendo clicado, se for ele deleta o item. */
       const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
   
       if (currentTaskIsBeingClicked) {
         taskItemContainer.remove();
       }
     }
-  
+  /* Executando função que armazena as alterações feitas no LocalStorage */
     updateLocalStorage();
   };
 /* Valida se o input está válido, se tiver, ele vai remover a classe error
@@ -81,20 +95,27 @@ const handleInputChange = () => {
         return inputElement.classList.remove('error');
     }
 };
-
+/* Função que armazena dados no localStorage */
 const updateLocalStorage = () => {
+    /* Variavel que armazena os itens da lista */
     const tasks = tasksContainer.childNodes;
-
+    /* criamos um map com todos os itens armazenados no tasks  */
+    /* e para cada task ele vai executar a função abaixo */
     const localStorageTasks = [... tasks].map(task => {
+        /* armazena o primeiro item na variavel content */
         const content = task.firstChild;
+        /* verifica se nesse primeiro item q foi armazenado no content contem o completed */
         const isCompleted = content.classList.contains('completed');
 
-        return { description: content.innerText, isCompleted };taskContent.addEventListener('click', () => handleClick(taskContent));
+        return { description: content.innerText, isCompleted };
+        taskContent.addEventListener('click', () => handleClick(taskContent));
     });
-
+    /* Função que armazena o item no localStorage */
+    /* Essa função JSON.stringify como o nome diz converter Json para String */
     localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
 };
 
+/* Essa função pega os dados salvos no localStorage e coloca na tela */
 const refreshTasksUsingLocalStorage = () => {
     const tasksFromLocalStorage = JSON.parse(localStorage.getItem('tasks'));
     
@@ -125,12 +146,12 @@ const refreshTasksUsingLocalStorage = () => {
         taskItemContainer.appendChild(deleteItem);
 
         tasksContainer.appendChild(taskItemContainer);
-    }/* Função que chama */
+    }
 };
-
+/* Função que chama */
 refreshTasksUsingLocalStorage();
 
-/* Chama a função handleAddTask caso clique no botão adicionar*/
+/* Essa função espera um clique para executar a função Handle Click */
 addTaskButton.addEventListener('click', () => handleAddTask());
 /* Chama a função handleInputCHange caso o input for válido, essa função removerá a classe error */
 inputElement.addEventListener('change', () => handleInputChange());
